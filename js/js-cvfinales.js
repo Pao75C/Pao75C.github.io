7b66d2914575ed38aa95289aab869bc37e102e5c
Pao75C <125836247+Pao75C@users.noreply.github.com>
2023-03-18 13:53:00 -0300

    Add files via upload

--- js/js-cvfinales.js
+++ js/js-cvfinales.js
@@ -1,14 +1,3 @@
-/*
-var boton = document.createElement("button");
-boton.innerHTML = "CONTACTO";
-
-var divContacto = document.getElementsByClassName("contacto")[0];
-divContacto.appendChild(boton);
-
-boton.addEventListener ("click", function() {
-  window.location.href = "mailto:jrodri67@gmail.com";
-});
-*/
 function show(shown, hidden1, hidden2) {
   document.getElementById(shown).style.display = 'block';
   document.getElementById(hidden1).style.display = 'none';
